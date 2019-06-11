925b634bd131baaa6228b2e0d34815192f42767d
Mickae Truman <mtruman@nyit.edu>
2019-06-11 00:25:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    @art_id = params[:id]
+    erb :edit
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params["id"])
+    @article.update(title: params["title"], content: params["content"])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.delete
+    redirect to '/articles'
+  end
+
 end
+    
+    
